f487594fc7edc2cf73b7d9970030f4feedec74b4
mohitgureja <gurejamohit.32@gmail.com>
2022-10-11 23:33:51 +0200

    Chanhes for V2

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import UploadFilesSection from "./components/UploadFilesSection";
 import axios from "axios";
 var FormData = require('form-data');
 
-const BASE_URL = "http:/127.0.0.1:5000/";
+const BASE_URL = "http:/54.89.53.157/";
 
 function App() {
   const [text, setText] = useState('')
--- src/components/UploadFilesSection/index.js
+++ src/components/UploadFilesSection/index.js
@@ -16,7 +16,7 @@ function UploadFilesSection() {
     </Box>
   ));
   
-  const BASE_URL = "http://127.0.0.1:5000/";
+  const BASE_URL = "http://54.89.53.157/";
 
   async function maskFiles(){
     console.log(uploadedFiles)
